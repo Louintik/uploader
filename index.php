4729fd0f8255f3490643963260db5a527bf9cf21
louisa <86868898768@mail.ru>
2025-06-03 18:50:52 +0200

    Настроила Докер

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//
+//require_once __DIR__ . '/..' . '/vendor/autoload.php';  //путь __DIR__ считается с текущей директории
+//
+//$routes = require_once __DIR__ . '/..' . '/config/routes.php';
+//
+//$uri = $_SERVER['REQUEST_URI'];
+//
+//$routes[$uri]();
+
+phpinfo();
+
